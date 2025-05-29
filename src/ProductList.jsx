df859034cd29072e10d8dfbc5d1406418036519e
opomeper <saurojoemer14@gmail.com>
2025-05-30 06:53:00 +0800

    Added CSS

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -311,8 +311,8 @@ function ProductList({ onHomeClick }) {
                 <div className="product-grid">
                     {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                         <div key={index}> {/* Unique key for each category div */}
-                            <h1>
-                                <div>{category.category}</div> {/* Display the category name */}
+                            <h1 className="plantname_heading">
+                                <div className="plant_heading" >{category.category}</div> {/* Display the category name */}
                             </h1>
                             <div className="product-list"> {/* Container for the list of plant cards */}
                                 {category.plants.map((plant, plantIndex) => ( // Loop through each plant in the current category
